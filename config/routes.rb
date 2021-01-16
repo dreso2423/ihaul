a9ab1e437e617d67f6c6b013a4125f97d4478574
Andre Sottil <dreso2423@gmail.com>
2021-01-16 17:01:46 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :trucks
+  resources :bookings, only: [:create]
+  resources :reviews, only: [:create]
+  end
+trucks (index, show, new,create, edit, update
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
