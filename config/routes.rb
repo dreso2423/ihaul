39371ff3aeb28ff8989de7aa18c24698d6f208af
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-01-19 19:30:48 -0800

    devise implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   resources :trucks
   resources :bookings, only: [:create]
   resources :reviews, only: [:create]
-  end
-trucks (index, show, new,create, edit, update
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
