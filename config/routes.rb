effdcdad56b71229826e4e3ff454294f264a51c6
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-01-19 19:52:14 -0800

    added trucks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'trucks/index'
+  get 'trucks/show'
+  get 'trucks/edit'
+  get 'trucks/new'
   root to: 'pages#home'
   resources :trucks
   resources :bookings, only: [:create]
   resources :reviews, only: [:create]
-  end
-trucks (index, show, new,create, edit, update
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/trucks_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TrucksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get trucks_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get trucks_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get trucks_edit_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get trucks_new_url
+    assert_response :success
+  end
+
+end
