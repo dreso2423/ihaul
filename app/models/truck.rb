836dd9a5c94d016d40ee3e1ed421eb38143a1f47
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-01-28 17:59:35 -0800

    added search feature

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -2,4 +2,12 @@ class Truck < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
