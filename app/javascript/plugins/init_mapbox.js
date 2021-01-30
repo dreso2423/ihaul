560618ec321e700e44eab2033014b376bc28866e
andre sottil <dreso2423@gmail.com>
2021-01-30 10:22:05 -0600

    good

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,3 +13,24 @@ const initMapbox = () => {
 };
 
 export { initMapbox };
+
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+  });
+};
+
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+// [...]
+if (mapElement) {
+  // [...]
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
+}
+
